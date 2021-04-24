33f70307cb2c84d1d2beb43ac5857a4790fa9348
dd_06 <dd06arceus@gmail.com>
2021-04-24 15:32:45 +0200

    Fix

--- Website/frontend/src/index.js
+++ Website/frontend/src/index.js
@@ -51,8 +51,3 @@ function updateRender() {
 }
 
 export { updateRender, cookies }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
