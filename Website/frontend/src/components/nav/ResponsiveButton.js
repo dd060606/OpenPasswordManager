ce93a431b01ddddaf3962199d0249eab7caf9505
dd_06 <dd06arceus@gmail.com>
2021-04-26 16:07:32 +0200

    Fix

--- Website/frontend/src/components/nav/ResponsiveButton.js
+++ Website/frontend/src/components/nav/ResponsiveButton.js
@@ -1,40 +1,21 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import ResponsiveNav from './ResponsiveNav';
 import "./css/ResponsiveButton.css"
 
 
 let setMenuOpen;
-const StyledResponsiveButton = styled.div`
 
-  
-  div {
-
-    background-color: ${({ open }) => open ? '#ccc' : '#333'};
-
-    &:nth-child(1) {
-      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
-    }
-    &:nth-child(2) {
-      transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
-      opacity: ${({ open }) => open ? 0 : 1};
-    }
-    &:nth-child(3) {
-      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
-    }
-  }
-`;
 
 const ResponsiveButton = () => {
   const [open, setOpen] = useState(false)
   setMenuOpen = setOpen
   return (
     <>
-      <StyledResponsiveButton className="responsive-btn" style={{ position: open ? "fixed" : "absolute" }} open={open} onClick={() => setOpen(!open)}>
-        <div />
-        <div />
-        <div />
-      </StyledResponsiveButton>
+      <div className="responsive-btn" style={{ position: open ? "fixed" : "absolute" }} open={open} onClick={() => setOpen(!open)}>
+        <div style={{ backgroundColor: open ? '#ccc' : '#333', transform: open ? 'rotate(45deg)' : 'rotate(0)' }} />
+        <div style={{ backgroundColor: open ? '#ccc' : '#333', transform: open ? 'translateX(100%)' : 'translateX(0)', opacity: open ? 0 : 1 }} />
+        <div style={{ backgroundColor: open ? '#ccc' : '#333', transform: open ? 'rotate(-45deg)' : 'rotate(0)' }} />
+      </div>
       <ResponsiveNav open={open} />
     </>
   )
