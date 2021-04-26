fb2adca6c1f7b8e995ff4147021135df8f9068d8
dd_06 <dd06arceus@gmail.com>
2021-04-26 17:15:42 +0200

    Started working on login page

--- Webapp/frontend/src/components/auth/Login.js
+++ Webapp/frontend/src/components/auth/Login.js
@@ -1,5 +1,7 @@
 import { Component } from "react"
 import "./css/Login.css"
+import { translate } from "../../utils/langManager"
+
 
 class Login extends Component {
 
@@ -9,7 +11,24 @@ class Login extends Component {
 
             <div className="auth-login">
 
+                <img src={`${process.env.PUBLIC_URL}/assets/images/logo.png`} alt="nav-logo" id="nav-logo" width={120} height={110} />
 
+                <div className="welcome-login">
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/icon.png`} alt="welcome-icon" className="icon" width={100} height={120} />
+
+                    <h1 className="welcome">{translate("welcome-on")} OpenPasswordManager</h1>
+                    <h3>{translate("please-connect-to-opm")}</h3>
+                </div>
+                <form className="login-form">
+
+                    <h2>{translate("login-to-opm")}</h2>
+                    <div className="email-field">
+                        <i className="fal fa-envelope field-icon"></i>
+                        <input type="email" placeholder={translate("email")} />
+
+                    </div>
+
+                </form>
             </div>)
     }
 }
--- Webapp/frontend/src/index.js
+++ Webapp/frontend/src/index.js
@@ -7,35 +7,43 @@ import Login from "./components/auth/Login"
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { setLanguage, setLangJSON } from "./utils/langManager"
+import Swal from 'sweetalert2'
 
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 
 if (cookies.get("language") === undefined) {
-  cookies.set('language', 'en', { path: '/' });
+  cookies.set('language', 'en', { path: '/' })
+  Swal.fire({
+    title: 'Please select a language',
+    input: 'select',
+    inputOptions: {
+      english: "English",
+      french: "Français"
+    },
+    inputPlaceholder: 'Select a language',
+    showCancelButton: true,
+    inputValidator: (value) => {
+      return new Promise((resolve) => {
+        if (value === "english" || value === "french") {
+          cookies.set("language", value.substring(0, 2).toLowerCase(), { path: '/' })
+          updateLanguage()
+          resolve()
+        }
+      })
+    }
+  })
 }
 
-fetch('assets/langs/' + cookies.get("language") + '.json', {
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
-
-})
-  .then(res => res.json())
-  .then(json => {
-    setLangJSON(json)
-    setLanguage(cookies.get("language"))
-    updateRender()
-  })
+updateLanguage()
 
 function updateRender() {
   ReactDOM.render(
     <React.StrictMode>
       <Router>
         <Switch>
-          <Route path="/" exact component={Login} />
+          <Route path="/Login" exact component={Login} />
           <Route path="/" component={Error404} />
         </Switch>
       </Router>
@@ -44,4 +52,20 @@ function updateRender() {
   );
 }
 
-export { updateRender, cookies }
+function updateLanguage() {
+  fetch('assets/langs/' + cookies.get("language") + '.json', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+  })
+    .then(res => res.json())
+    .then(json => {
+      setLangJSON(json)
+      setLanguage(cookies.get("language"))
+      updateRender()
+    })
+}
+
+export { updateRender, cookies, updateLanguage }
