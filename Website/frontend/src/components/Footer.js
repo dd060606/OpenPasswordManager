709312cc47d342a9e7af9ea1fbb1c6eaf7426c18
dd_06 <dd06arceus@gmail.com>
2021-04-26 11:27:27 +0200

    Fix

--- Website/frontend/src/components/Footer.js
+++ Website/frontend/src/components/Footer.js
@@ -9,9 +9,7 @@ class Footer extends Component {
         return (
             <footer>
                 <a href="https://github.com/dd060606/OpenPasswordManager" rel="noopener noreferrer" target="_blank"><i className="fab fa-github" /> GitHub</a>
-                <hr />
                 <p className="message-cookies">{translate("essential-cookies")}</p>
-                <hr />
                 <p >{translate("created-by")} <strong><a href="https://github.com/dd060606" rel="noopener noreferrer" target="_blank">dd_06</a></strong></p>
             </footer >)
     }
